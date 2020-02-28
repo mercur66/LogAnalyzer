347d1648641d5cb94f8b736b06047b22c0cf638b
JUNHO <mercur66@naver.com>
2020-02-28 16:58:57 +0900

    db insert

--- test.py
+++ test.py
@@ -8,19 +8,22 @@ data = pd.read_csv('access.log52063'
                     , names = ['ip', 'empty1', 'empty2', 'time', 'type', 'url', 'status', 'size', 'a']
                     )
 
-con = sqlite3.connect("C:/workspace/Python/Pyth37Start/python-studylog_analyze_db.db") #로그분석 DB파일 생성, 없으면 생성해주고 있으면 가져온다
+con = sqlite3.connect("C:/workspace/Python/LogAnalyzer/DB/log_analyze_db.db") #로그분석 DB파일 생성, 없으면 생성해주고 있으면 가져온다
 cursor = con.cursor()
 
 #sql = "CREATE TABLE ACCESS_LOG(IP TEXT, TIME TEXT, TYPE TEXT, URL TEXT, STATUS INTEGER, SIZE INTEGER)"
 #cursor.execute(sql)
 #AAAAAAAAffffffff
-print(data[['ip', 'time']])
+#print(data[['ip', 'time']])
 
 #sql = "INSERT INTO ACCESS_LOG VALUES('10.60.30.68', '2020-02-17', 'GET', 'www.ourhome.co.kr', 200, 10)"
 #cursor.execute(sql)
 
-sql = "SELECT * FROM ACCESS_LOG"
+#sql = "SELECT * FROM ACCESS_LOG"
 #print(cursor.execute(sql).fetchall())
+
+data.to_sql(name="TB_LOG", con=con, if_exists='append')
+
 
 con.commit()
 con.close()
