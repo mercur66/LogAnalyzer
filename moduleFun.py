b6c3870cf21d6fb86c641bab34aa47625be90086
hsh92 <hsh926149@gmail.com>
2020-03-06 17:16:34 +0900

    20200304_add

---
+++ moduleFun.py
@@ -0,0 +1,15 @@
+def cvMonthNum(vMonth):
+
+    return {'Jan': '01',
+            'Feb': '02',
+            'Mar': '03',
+            'Apr': '04',
+            'May': '05',
+            'Jun': '06',
+            'Jul': '07',
+            'Aug': '08',
+            'Sept': '09',
+            'Oct': '10',
+            'Nov': '11',
+            'Dec': '12'
+            }[vMonth]
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+import moduleFun as md
 
 data = pd.read_csv('access.log52063'
                     , engine='python'
@@ -22,7 +23,60 @@ cursor = con.cursor()
 #  SIZE : 343
 #  InpuDate :"20200302"
 
-data.to_sql(name="TB_LOG", con=con, if_exists='append')
+# data.to_sql(name="TB_LOG", con=con, if_exists='replace')
+
+# if_exits
+# fail: Raise a ValueError.
+# replace: Drop the table before inserting new values.#
+# append: Insert new values to the existing table.
+
+sql = "SELECT * FROM TB_LOG where rowid <= 10 "
+# cursor.execute(sql).fetchall()
+cursor.execute(sql)
+
+for row in cursor:
+    print("===========================================")
+
+    print(row)
+
+    vIp =  row[1]
+
+    vTemp = row[4].split('/')
+    vDay = vTemp[0].replace('[','')
+    vMon = md.cvMonthNum(vTemp[1])
+    vYear = vTemp[2].split(':')[0]
+    vHH = vTemp[2].split(':')[1]
+    vMM = vTemp[2].split(':')[2]
+    vSS = vTemp[2].split(':')[3]
+    vDateTime =  vYear+vMon+vDay+vHH+vMM+vSS
+    timeZone = row[5].replace(']', '')
+
+    print("IP : " + row[1])
+    print("vTemp : " + str(vTemp))
+    print("DateTime : " + vDateTime )
+
+    print("timeZone : " + timeZone)
+
+
+    splitURL = row[6].split()
+
+    print("P/G : " + splitURL[0])
+    print("URL : " + splitURL[1])
+    print("PROTOCOL  : " + splitURL[2])
+    print("STATUS : " + str(row[7]))
+    print("SIZE : " + str(row[8]))
+
+    print("===========================================")
+
+# 파일 전처리  : data 객체에서 초기값으로 들어온 객체를 하기의 항목으로 나눠서 임시 dataTemp 객체에 넣는다.
+#  IP   : "15.3.3.1"
+#  date : '20200101123160' --YYYYMMDDHHMMSS
+#  P/G  : "POST"
+#  URL  : "/portal/approval/work/getSoaUndoCount.do"
+#  PROTOCOL : "HTTP/1.1 "
+#  STATUS : 200
+#  SIZE : 343
+#  InpuDate :"20200302"
 
 con.commit()
 con.close()
